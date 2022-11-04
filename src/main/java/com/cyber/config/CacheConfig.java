153c6ad3006edf469e13e52424c6335f4e123dcb
Janusjia85 <531907642@qq.com>
2022-11-04 17:47:51 +0800

    review

---
+++ src/main/java/com/cyber/config/CacheConfig.java
@@ -0,0 +1,84 @@
+package com.cyber.config;
+
+
+import com.cyber.entity.Entity;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.Objects;
+
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+@EnableCaching
+public class CacheConfig extends CachingConfigurerSupport {
+
+    public static final Logger LOGGING = LoggerFactory.getLogger(CacheConfig.class);
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    @Override
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return (obj, method, args) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(obj.getClass().getName());
+            for (Object arg : args) {
+                if (Objects.nonNull(arg) && !(arg instanceof Entity)) {
+                    sb.append(arg.toString());
+                }
+            }
+            return sb.toString();
+        };
+    }
+
+    @Bean
+    public CacheManager cacheManager(LettuceConnectionFactory redisConnectionFactory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.activateDefaultTyping(om.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
+        serializer.setObjectMapper(om);
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/cyber/utils/RedisLock.java
@@ -0,0 +1,77 @@
+package com.cyber.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.connection.ReturnType;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+public class RedisLock {
+
+    public static final Logger LOGGING = LoggerFactory.getLogger(RedisLock.class);
+
+    private static final long DEFAULT_EXPIRE = 30*1000L;
+    public static final String UNLOCK_LUA;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    public RedisLock(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    static {
+        StringBuilder sb = new StringBuilder();
+        sb.append("if redis.call(\"get\",KEYS[1]) == ARGV[1] ");
+        sb.append("then ");
+        sb.append("    return redis.call(\"del\",KEYS[1]) ");
+        sb.append("else ");
+        sb.append("    return 0 ");
+        sb.append("end ");
+        UNLOCK_LUA = sb.toString();
+    }
+
+
+
+    public boolean lock(String lockKey, String lockValue) {
+        return this.lock(lockKey, lockValue, DEFAULT_EXPIRE, TimeUnit.MILLISECONDS);
+    }
+
+
+    public boolean lock(String lockKey, String lockValue, long expire, TimeUnit timeUnit) {
+        try {
+            RedisCallback<Boolean> callback = (connection) -> connection.set(lockKey.getBytes(StandardCharsets.UTF_8),
+                    lockValue.getBytes(StandardCharsets.UTF_8), Expiration.seconds(timeUnit.toSeconds(expire)),
+                    RedisStringCommands.SetOption.SET_IF_ABSENT);
+            return redisTemplate.execute(callback);
+        } catch (Exception e) {
+            LOGGING.error("redis lock error ,lock key: {}, value : {}, error info : {}", lockKey, lockValue, e);
+        }
+        return false;
+    }
+
+
+    public boolean unlock(String lockKey, String lockValue) {
+        RedisCallback<Boolean> callback = (connection) -> connection.eval(UNLOCK_LUA.getBytes(), ReturnType.BOOLEAN, 1, lockKey.getBytes(StandardCharsets.UTF_8), lockValue.getBytes(StandardCharsets.UTF_8));
+        return redisTemplate.execute(callback);
+    }
+
+
+    public String get(String lockKey) {
+        try {
+            RedisCallback<String> callback = (connection) -> new String(Objects.requireNonNull(connection.get(lockKey.getBytes())), StandardCharsets.UTF_8);
+            return redisTemplate.execute(callback);
+        } catch (Exception e) {
+            LOGGING.error("get redis value occurred an exception,the key is {}, error is {}", lockKey, e);
+        }
+        return null;
+    }
+}
