ea6d25e58e9ae0e399022097441d53ba68ccb8f8
Janusjia85 <531907642@qq.com>
2022-11-06 13:52:10 +0800

    add jetcache config

---
+++ src/main/java/com/alicp/jetcache/anno/JsonCacheObject.java
@@ -0,0 +1,30 @@
+package com.alicp.jetcache.anno;
+
+public class JsonCacheObject<V> {
+    private String className;
+    private V object;
+
+    public JsonCacheObject() {
+    }
+
+    public JsonCacheObject(String className, V object) {
+        this.className = className;
+        this.object = object;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public V getObject() {
+        return object;
+    }
+
+    public void setObject(V object) {
+        this.object = object;
+    }
+}
